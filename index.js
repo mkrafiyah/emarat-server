f4b404240364de73ce3b6f5fe29db1e43be6914e
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-02 20:02:08 +0400

    users get api added

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ async function run() {
 
 
     //user
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users', async(req, res)=>{
       const user = req.body;
       const query = {email: user.email}
