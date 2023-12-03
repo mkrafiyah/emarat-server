198223185915813e387b8bc800119ac6257742a4
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-03 22:14:47 +0400

    data changed

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ async function run() {
     const result = await wishCollection.insertOne(wishItem);
     res.send(result);
  })
+ //wish delete
  app.delete('/wishes/:id', async(req, res)=>{
   const id = req.params.id;
   const query = {_id: new ObjectId(id)};
@@ -172,7 +173,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
