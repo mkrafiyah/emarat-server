a77e05b773f644527dbc9b040821b6c8de50fd72
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-03 22:10:27 +0400

    client line comment

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const propertyCollection = client.db("emaratDb").collection("properties");
     const reviewCollection = client.db("emaratDb").collection("reviews");
